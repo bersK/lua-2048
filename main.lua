e57f84aa7d06f1c0384c26044d1c3f7274ddd42c
Stefan Stefanov <stefan.karolev1@gmail.com>
2022-01-15 00:18:59 +0200

    Basic main file

---
+++ main.lua
@@ -0,0 +1,15 @@
+require("grid")
+
+local grid = Grid.new()
+
+print(grid:withinBounds({x = 3, y = 3}))
+
+function love.keyreleased(key)
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+end
