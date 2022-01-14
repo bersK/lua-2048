2e06f37c39dd64ad60bf0a3b48cda349f994aedc
Stefan Stefanov <stefan.karolev1@gmail.com>
2022-01-15 01:37:22 +0200

    Testing grid

--- main.lua
+++ main.lua
@@ -12,4 +12,10 @@ function love.update(dt)
 end
 
 function love.draw()
+    local offset = 25
+    for x = 0, 3, 1 do
+        for y = 0, 3, 1 do
+            love.graphics.rectangle("fill", offset + x*135 + x*5, offset + y*135 + y*5, 135, 135, 8)
+        end
+    end
 end
