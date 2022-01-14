08e31fda391929831494ad510eebd1b7accd26c0
Stefan Stefanov <stefan.karolev1@gmail.com>
2022-01-15 01:54:43 +0200

    Test layout for the game and custom font added

--- main.lua
+++ main.lua
@@ -2,7 +2,6 @@ require("grid")
 
 local grid = Grid.new()
 
-print(grid:withinBounds({x = 3, y = 3}))
 
 function love.keyreleased(key)
 end
@@ -12,6 +11,13 @@ function love.update(dt)
 end
 
 function love.draw()
+    local bestScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 30)
+    local currentScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 55)
+    -- love.graphics.print("Is within bounds "..tostring(grid:withinBounds({x = 3, y = 3})), font, 35, 615)
+    love.graphics.print("best score", bestScoreFont, 35, 615)
+    love.graphics.print("1200", bestScoreFont, 400, 615)
+    love.graphics.print("score", currentScoreFont, 35, 675)
+    love.graphics.print("3000", currentScoreFont, 370, 675)
     local offset = 25
     for x = 0, 3, 1 do
         for y = 0, 3, 1 do
