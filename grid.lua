4ae888295a4bb8d0fb029c1493e79eb00373492d
Stefan Stefanov <stefan.karolev1@gmail.com>
2022-01-15 16:47:47 +0200

    Formatting grid file

--- grid.lua
+++ grid.lua
@@ -3,44 +3,52 @@ require("tile")
 Grid = {}
 Grid.__index = Grid
 
-
 function Grid.new()
-    local o = setmetatable({size = 4, cells = {}}, Grid)
+    local o = setmetatable({size = 4, tiles = {}}, Grid)
     o.tileFont = love.graphics.newFont("fonts/retro_font.TTF", 50)
+    o.tileFont:setFilter("nearest", "nearest")
+
+    for x = 1, o.size, 1 do
+        o.tiles[x] = {}
+        for y = 1, o.size, 1 do o.tiles[x][y] = nil end
+    end
+
     return o
 end
 
 function Grid:withinBounds(position)
-  return position.x >= 0 and position.x < self.size and
-         position.y >= 0 and position.y < self.size
+    return position.x >= 1 and position.x <= self.size and position.y >= 1 and
+               position.y <= self.size
 end
 
 function Grid:isFreeSpace(position)
-  return self.cells[position.x][position.y] == nil
+    return self.tiles[position.x][position.y] == nil
 end
 
 function Grid:insertTile(position, number)
-  self.cells[position.x][position.y] = Tile.new(number)
+    self.tiles[position.x][position.y] = Tile.new(number)
 end
 
-function Grid:remoteTile(position)
-  self.cells[position.x][position.y] = nil
-end
+function Grid:removeTile(position) self.tiles[position.x][position.y] = nil end
 
+function Grid:moveGrid(direction) end
 -- Moves the tile at the position and returns if it was successful
-function Grid:moveTile(position, direction)
-end
+function Grid:moveTile(position, direction) end
 
 function Grid:draw()
+    love.event.push("custom_evt", 10)
+    local offset = 25
+    for x = 0, 3, 1 do
+        for y = 0, 3, 1 do
 
-  local offset = 25
-  for x = 0, 3, 1 do
-    for y = 0, 3, 1 do
-      love.graphics.setColor(255,255,255,255)
-      love.graphics.rectangle("fill", offset + x*135 + x*5, offset + y*135 + y*5, 135, 135, 8)
-      love.graphics.setColor(0,0,0,255)
-      love.graphics.print("128", self.tileFont, offset + x*135 + x*5 + 5, offset + y*135 + y*5 + 50)
+            love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.rectangle("fill", offset + x * 135 + x * 5,
+                                    offset + y * 135 + y * 5, 135, 135, 8)
+            love.graphics.setColor(0, 0, 0, 255)
+            love.graphics.print("128", self.tileFont,
+                                offset + x * 135 + x * 5 + 5,
+                                offset + y * 135 + y * 5 + 50)
+        end
     end
-  end
-  love.graphics.setColor(255,255,255,255)
+    love.graphics.setColor(255, 255, 255, 255)
 end
