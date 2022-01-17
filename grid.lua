028f5146a4c7f0e8a48aaa382fbfbc7e1208ed90
Stefan Stefanov <stefan.karolev1@gmail.com>
2022-01-17 21:51:32 +0200

    Loading score and saving

--- grid.lua
+++ grid.lua
@@ -1,10 +1,12 @@
 require("tile")
+local serialize = require("lib/ser")
 
 Grid = {}
 Grid.__index = Grid
 
+-- Create a grid object and return
 function Grid.new()
-    local o = setmetatable({size = 4, tiles = {}, gameStarted = false}, Grid)
+    local o = setmetatable({size = 4, tiles = {}, gameStarted = false, currentScore = 0, bestScore = 0}, Grid)
     -- Populate the grid with tiles
     for x = 1, o.size, 1 do
         o.tiles[x] = {}
@@ -13,32 +15,35 @@ function Grid.new()
         end
     end
 
+    o:loadScore()
     o:generateStartingGrid()
     return o
 end
 
+-- Check if coordinates are within the grid and returns a bool
 function Grid:withinBounds(position)
     return position.x >= 1 and position.x <= self.size and position.y >= 1 and
                position.y <= self.size
 end
 
+-- Reset all tiles to be the value 0 (blanks)
 function Grid:resetGrid()
     for x = 1, self.size, 1 do
         self.tiles[x] = {}
-        for y = 1, self.size, 1 do 
+        for y = 1, self.size, 1 do
             self.tiles[x][y] = Tile.new(0, {x = x, y = y})
         end
     end
-    
 end
 
+-- Generate a starting board (2 tiles)
 function Grid:generateStartingGrid()
     math.randomseed(os.time())
     local x1 = math.random(1, self.size)
     local y1 = math.random(1, self.size)
     local x2 = ((x1 + 1) % self.size) + 1
     local y2 = ((y1 + 1) % self.size) + 1
-    
+
     -- self:insertTile(x1, y1, 2)
     -- self:insertTile(x2, y2, 2)
     self:insertTile(1, 1, 4)
@@ -51,30 +56,30 @@ function Grid:generateStartingGrid()
     self:insertTile(4, 3, 2)
 end
 
+-- Chekc if the tile is free or not (a free tile has a number 0)
 function Grid:isFreeSpace(position)
     return self.tiles[position.x][position.y].number == 0
 end
 
+-- Insert a new tile on the position
 function Grid:insertTile(x, y, number)
     self.tiles[x][y] = Tile.new(number, {x = x, y = y})
 end
 
-function Grid:removeTile(position)
-    self.tiles[position.x][position.y].number = 0
- end
+-- Increase the current score of the game (no side effects, abs used on parameter)
+function Grid:addToCurrentScore(number)
+    self.currentScore = self.currentScore + math.abs(number)
+end
 
-function Grid:moveGrid(direction) end
--- Moves the tile at the position and returns if it was successful
-function Grid:moveTile(position, direction) end
-
+-- Move the tiles left on the grid
 function Grid:moveLeft()
     for y = 1, 4, 1 do
         for x = 1, 3, 1 do
             local currTileNumber = self.tiles[x][y].number
             local nextTileNumber = self.tiles[x+1][y].number
             if currTileNumber == nextTileNumber and currTileNumber ~= 0 then
-                -- print("Sam; number! T1", x, y, "T2:", x+1, y)
                 self:insertTile(x, y, currTileNumber*2)
+                self:addToCurrentScore(currTileNumber*2)
                 self:insertTile(x+1, y, 0)
             end
         end
@@ -91,14 +96,15 @@ function Grid:moveLeft()
     end
 end
 
+-- Move the tiles right on the grid
 function Grid:moveRight()
     for y = 1, 4, 1 do
         for x = 4, 2, -1 do
             local currTileNumber = self.tiles[x][y].number
             local nextTileNumber = self.tiles[x-1][y].number
             if currTileNumber == nextTileNumber and currTileNumber ~= 0 then
-                -- print("Same number! T1", x, y, "T2:", x-1, y)
                 self:insertTile(x, y, currTileNumber*2)
+                self:addToCurrentScore(currTileNumber*2)
                 self:insertTile(x-1, y, 0)
             end
         end
@@ -115,6 +121,7 @@ function Grid:moveRight()
     end
 end
 
+-- Move the tiles up on the grid
 function Grid:moveUp()
     for x = 1, 4, 1 do
         for i = 1, 4 do
@@ -130,16 +137,16 @@ function Grid:moveUp()
         for y = 1, 3, 1 do
             local currTileNumber = self.tiles[x][y].number
             local nextTileNumber = self.tiles[x][y+1].number
-            -- print(x, y)
             if currTileNumber == nextTileNumber and currTileNumber ~= 0 then
-                -- print("Same number! T1", x, y, "T2:", x, y+1)
                 self:insertTile(x, y, currTileNumber*2)
+                self:addToCurrentScore(currTileNumber*2)
                 self:insertTile(x, y+1, 0)
             end
         end
     end
 end
 
+-- Move the tiles down on the grid
 function Grid:moveDown()
     for x = 1, 4, 1 do
         for i = 1, 4 do
@@ -155,20 +162,36 @@ function Grid:moveDown()
         for y = 4, 2, -1 do
             local currTileNumber = self.tiles[x][y].number
             local nextTileNumber = self.tiles[x][y-1].number
-            -- print(x, y)
             if currTileNumber == nextTileNumber and currTileNumber ~= 0 then
-                -- print("Same number! T1", x, y, "T2:", x, y-1)
                 self:insertTile(x, y, currTileNumber*2)
+                self:addToCurrentScore(currTileNumber*2)
                 self:insertTile(x, y-1, 0)
             end
         end
     end
 end
 
+-- Draw all the tiles
 function Grid:draw()
     for x = 1, self.size, 1 do
         for y = 1, self.size, 1 do
             self.tiles[x][y]:draw({x = x, y = y})
         end
     end
+end
+
+-- Load the score from a local file
+function Grid:loadScore()
+    if not love.filesystem.getInfo("savegame.txt") then
+        self.bestScore = 0
+        self:saveScore()
+    end
+    local data = love.filesystem.load("savegame.txt")()
+    self.bestScore = data.maxScore
+end
+
+function Grid:saveScore()
+    local data = {}
+    data.maxScore = math.max(self.currentScore, self.bestScore)
+    love.filesystem.write("savegame.txt", serialize(data))
 end
--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ require("grid")
 
 local grid = Grid.new()
 
+local bestScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 30)
+local currentScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 55)
 
 function love.keyreleased(key)
     if key == "escape" or key == "q" then
+        grid:saveScore()
         love.event.push("quit")
     end
 
@@ -31,12 +34,10 @@ function love.update(dt)
 end
 
 function love.draw()
-    local bestScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 30)
-    local currentScoreFont = love.graphics.newFont("fonts/retro_font.TTF", 55)
     love.graphics.setColor({1, 1, 1, 1})
     love.graphics.print("best score", bestScoreFont, 35, 615)
-    love.graphics.print("1200", bestScoreFont, 400, 615)
+    love.graphics.print(tostring(grid.bestScore), bestScoreFont, 400, 615)
     love.graphics.print("score", currentScoreFont, 35, 675)
-    love.graphics.print("3000", currentScoreFont, 370, 675)
+    love.graphics.print(grid.currentScore, currentScoreFont, 370, 675)
     grid:draw()
 end
